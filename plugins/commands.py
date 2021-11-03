f7bdf4c58368361a1ade01505ec9e495bf982455
MessiArmy <92917165+MessiArmy@users.noreply.github.com>
2021-11-03 20:39:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -105,7 +105,7 @@ async def start(client, message):
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
         try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+            f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_caption=f_caption)
         except Exception as e:
             print(e)
             f_caption=f_caption
